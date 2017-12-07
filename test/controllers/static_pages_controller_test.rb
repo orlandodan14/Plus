4fe12e3a978dc40073198144dbd5107f646baaa1
Orlando <orlando.dan14@gmail.com>
2017-12-07 15:40:26 -0300

    static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,39 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Plus"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Plus-Home"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Plus-Home"
   end
 
   test "should get blog" do
     get static_pages_blog_url
     assert_response :success
+    assert_select "title", "Plus-Blog"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "Plus-About"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Plus-Contact"
   end
 
 end
