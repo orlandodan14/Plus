48fedcf91cab80d1a1dd9f9a7ea004d25b725156
Orlando <orlando.dan14@gmail.com>
2017-12-14 16:25:10 -0300

    Sign up page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,15 +1,26 @@
 class StaticPagesController < ApplicationController
 
   def home
+  	new_user
   end
 
   def blog
+  	new_user
   end
 
   def about
+  	new_user
   end
 
   def contact
+  	new_user
   end
   
+
+  private
+
+  	# Ser @user for the modal in all pages
+  	def new_user
+  		@user = User.new
+  	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,21 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"      
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
